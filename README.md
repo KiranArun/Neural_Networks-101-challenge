3dd29ecafeb01acec4973b27723e8994b86cc9e7
KiranArun <kironni@gmail.com>
2018-04-25 10:21:24 +0100

    added setup scripts

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Neural_Networks-101-challenge
+
+## WIP
+
 A challenge to create a model from a starter notebook to achieve highest accuracy on a cat vs dog images dataset
---
+++ scripts/setup.py
@@ -0,0 +1,9 @@
+import subprocess
+
+subprocess.call(['chmod', '+x', '/content/Neural_Networks-101-challenge/scripts/get_data.sh'])
+
+subprocess.call(['chmod', '+x', '/content/Neural_Networks-101-challenge/scripts/run_tensorboard.sh'])
+
+subprocess.call(['/content/Neural_Networks-101-challenge/scripts/get_data.sh'])
+
+subprocess.call(['/content/Neural_Networks-101-challenge/scripts/run_tensorboard.sh'])
