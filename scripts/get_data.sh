1a249138994d0b39940f84703d8136714a00ff05
KiranArun <kironni@gmail.com>
2018-04-27 10:12:03 +0100

    moved logdir to /content/ and made unzip data quiet

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -2,7 +2,7 @@
 
 wget https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip -P /content/Neural_Networks-101-challenge/data/
 
-unzip /content/Neural_Networks-101-challenge/data/kagglecatsanddogs_3367a.zip -d /content/Neural_Networks-101-challenge/data/
+unzip -qq /content/Neural_Networks-101-challenge/data/kagglecatsanddogs_3367a.zip -d /content/Neural_Networks-101-challenge/data/
 
 mkdir /content/Neural_Networks-101-challenge/data/train
 mkdir /content/Neural_Networks-101-challenge/data/validation
