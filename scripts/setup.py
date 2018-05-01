371e925328cbbe4413423283da60d421be0e9c9c
KiranArun <kironni@gmail.com>
2018-05-01 19:43:35 +0100

    changed location of logdir

--- scripts/setup.py
+++ scripts/setup.py
@@ -8,4 +8,4 @@ subprocess.call(['/content/Neural_Networks-101-challenge/scripts/get_data.sh'])
 
 subprocess.call(['/content/Neural_Networks-101-challenge/scripts/run_tensorboard.sh'])
 
-subprocess.call(['mkdir', '/content/Neural_Networks-101-challenge/logdir/'])
+subprocess.call(['mkdir', '/content/logdir/'])
